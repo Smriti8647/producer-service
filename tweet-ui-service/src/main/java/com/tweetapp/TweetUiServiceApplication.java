62d000dd722eca7b00bed34598993ed33fbe66da
Smriti <63850511+Smriti8647@users.noreply.github.com>
2022-05-12 13:47:05 +0530

    Added ui service

---
+++ tweet-ui-service/src/main/java/com/tweetapp/TweetUiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tweetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetUiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetUiServiceApplication.class, args);
+	}
+
+}
