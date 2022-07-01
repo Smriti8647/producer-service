5188836058fb33c33cd13e117e743e65980b76a0
unknown <cogjava65@iiht.tech>
2022-07-01 15:48:18 +0530

    fixing tag issues

--- tweet-ui-service/src/main/java/com/tweetapp/kafka/config/KafkaProducerConfiguration.java
+++ tweet-ui-service/src/main/java/com/tweetapp/kafka/config/KafkaProducerConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -17,11 +18,14 @@ import com.tweetapp.model.Tag;
 @Configuration
 public class KafkaProducerConfiguration {
 
+	@Value("${bootstrap-servers-config}")
+	private String serverAddress;
+
     @Bean
     public ProducerFactory<String, Tag> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverAddress);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
